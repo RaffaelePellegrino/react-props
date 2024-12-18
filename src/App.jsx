630af9827bf89a205bc5f80ea1e783fa40ab0599
RaffaelePellegrino <waffleaffumicato@gmail.com>
2024-12-18 08:49:08 +0100

    props added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,65 @@
 import Card from './components/card'
 import './index.css'
+import './App.css'
+const posts = [
+  {
+    id: 1,
+    title: "Titolo del Post",
+    image: '',
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["html", "css"],
+    published: true,
+  },
+  {
+    id: 2,
+    title: "Titolo del Post",
+    image: '',
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["js", "css"],
+    published: true,
+  },
+  {
+    id: 3,
+    title: "Titolo del Post",
+    image: '',
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["js", "php"],
+    published: true,
+  },
+  {
+    id: 4,
+    title: "Titolo del Post",
+    image: '',
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["html"],
+    published: false,
+  },
+];
+
 function App() {
 
   return (
-    <div className='App'>
-      <Card/>
-    </div>
-    
-  )
+    <div className="App">
+      <div className='box-card'>
+        {posts
+          .filter(post => post.published)  // Mostra solo i post pubblicati
+          .map(post => (
+            <Card 
+              key={post.id}
+              title={post.title}
+              image={post.image}
+              content={post.content}
+              tags={post.tags}
+            />
+        ))}
+      </div>
+
+  </div>
+);
 }
     
 export default App
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,14 +1,21 @@
 import './cardstyle.css'
-function Card(){
+function Card({ title, image, content, tags }){
     return(
         <div className="card">
             <div className="a60">
-                <img src="https://images.unsplash.com/photo-1524889777220-eae0b973ec80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                <img src={image}/>
             </div>
             <div className="a40">
-                <h2>Titolo del Post</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo temporibus, dolorum minima ratione vitae molestiae explicabo natus magnam vel perferendis.</p>
-                <button>Leggi di più</button>
+                <h2>{title}</h2>
+                <p>{content}</p>
+                <div className='containerTags'>
+                    {tags.map((tag, index) =>{
+                        return(
+                        <p key={index} className={tag}><strong>{tag} </strong></p>
+                    )
+                    })}
+                </div>
+                
             </div>
         </div>
     )
